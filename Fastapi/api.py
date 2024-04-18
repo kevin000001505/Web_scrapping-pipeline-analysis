3b4664314b60a32f23f8ba12c1a3a622e362e134
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-04-18 16:17:32 +0800

    start to build api

--- Fastapi/api.py
+++ Fastapi/api.py
@@ -3,10 +3,19 @@ import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine, engine
 
+region = input("Enter a region: ")
+start_date = input("Enter a start date: ")
+end_date = input("Enter an end date: ")
 
-def get_postgresql_conn() -> engine.base.connection:
-    
-
+def get_postgresql_conn():
+    connection = psycopg2.connect(
+        user = "postgres",
+        password = '0105',
+        host = "localhost",
+        port = 5432,
+        database = "earthquake_db"
+    )
+    return connection.cursor()
 
 
 
@@ -14,3 +23,21 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/earthquake_info")
+def earthquake_info(
+    region: str = "",
+    start_date: str = "",
+    end_date: str = ""
+):
+    print('successful')
+    sql = f"""
+    select * from earthquake
+    where region = '{region}'
+    and date >= '{start_date}'
+    and date <= '{end_date}'
+    """
+    postgre_conn = get_postgresql_conn()
+    data_df = pd.read_sql(sql, con = postgre_conn)
+    data_dict = data_df.to_dict(orient = 'records')
+    return {'date': data_dict}
